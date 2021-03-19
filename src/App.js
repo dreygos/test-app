e4d87f3bba6dbd0277ae6309a885f1a60e6f0092
Andrei Budoi <77269422+andreigoosfraba@users.noreply.github.com>
2021-03-19 16:10:11 +0200

    Update paragraph

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Looking for a React dev. Email as at <a href="mailto:admin@kontador.ro">admin@kontador.ro</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
